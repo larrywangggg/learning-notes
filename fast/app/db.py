32039f25f8179fd6616442e798c523bd1deaa09a
larrywang <u7807744@anu.edu.au>
2026-01-04 23:42:49 +1100

    implement @app.post using imagekitio

--- fast/app/db.py
+++ fast/app/db.py
@@ -4,13 +4,17 @@ import uuid
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclaritiveBase, relationship
-import datetime
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db" #change later to something like postgresql if needed
 
-class Post(DeclaritiveBase):
-    __tableame__ = "posts"
+class Base(DeclarativeBase): #sqlalchemy base class
+    pass
+
+
+class Post(Base): #define Post table
+    __tablename__ = "posts"
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4) # Generate a unique identifier for each post, as a primary key
     column = Column(Text)
@@ -25,7 +29,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn: # conn is short for connection
-        await conn.run_sync(DeclaritiveBase.metadata.create_all) # find all classes that inherit from DeclaritiveBase and create tables for them
+        await conn.run_sync(Base.metadata.create_all) # find all classes that inherit from DeclarativeBase and create tables for them
         
         
 
